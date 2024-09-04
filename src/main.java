4677026e3ee96d97d022c4d288ff1e7bafd0b488
foagura <foagura_0416@yahoo.co.jp>
2024-09-04 20:29:37 +0900

    first commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args){
+        int num1 = 10;
+        long num2 = 30;
+        long num3 = 10000000000L;
+        float num4 = 10.0f;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+    }
+}
